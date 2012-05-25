d9a6683c6487d5e68af95218cdb29b246feab34a
Yang Liu <lywander@gmail.com>
2012-05-24 18:03:28 -0700

    update

---
+++ SketchFlowPrototyping/Prototype1/Prototype1/Prototype1Screens/FormBuilder_ver_2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Prototype1Screens
+{
+	public partial class FormBuilder_ver_2 : UserControl
+	{
+		public FormBuilder_ver_2()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
